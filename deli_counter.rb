f337df8c209a799f30d241777f871c45f068551e
jpkim921 <jpkim921@gmail.com>
2018-01-15 22:24:35 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,8 @@
 
 def line(array)
 
-  line_arr = array.each_with_index { |e,i| "#{i + 1}. #{e}"}
-
+  line_arr = array.map.each_with_index { |e,i| "#{i + 1}. #{e}"}
+  
   if array.length == 0
     puts "The line is currently empty."
   else
